d3ad42b711477b204a95a7013f89db0a43243f86
Sarah Donato <47866580+CrimsonSarah@users.noreply.github.com>
2024-12-16 19:58:37 -0300

    Update admin.py

--- contact/admin.py
+++ contact/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
+from django.utils.timezone import now
 from contact.models import Contact
-from contact.models import Contact
+
 
 
 class ContactModelAdmin(admin.ModelAdmin):
